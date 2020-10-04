6bcfeaa21dade65653d33e1b3d913a828b300d7a
din wan <dinvvan@gmail.com>
2020-10-04 20:29:00 +0700

    Updates

---
+++ precache-manifest.b98f098493b11e71c5c3b8f3526dab4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a3100a906ec4de3de07a5a267d5ff8",
+    "url": "/react-from-nerd/index.html"
+  },
+  {
+    "revision": "9e2554e17e0c398c29df",
+    "url": "/react-from-nerd/static/css/main.83469024.chunk.css"
+  },
+  {
+    "revision": "078c2e9a1e2560361a19",
+    "url": "/react-from-nerd/static/js/2.2e997e14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-from-nerd/static/js/2.2e997e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2554e17e0c398c29df",
+    "url": "/react-from-nerd/static/js/main.7c5e7e7d.chunk.js"
+  },
+  {
+    "revision": "514a913cc9a3eee47938",
+    "url": "/react-from-nerd/static/js/runtime-main.a608fbad.js"
+  }
+]);
